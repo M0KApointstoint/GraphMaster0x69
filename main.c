d0333459d27402dd1720e464ec96c594942a2c77
Cristian Mocanu <mocanuc579@gmail.com>
2026-05-18 18:24:42 +0300

    Modifica testele.

--- main.c
+++ main.c
@@ -1,8 +1,42 @@
+#include <stdio.h>
 #include "liste_simplu_inlantuite.h"
 #include "grafuri.h"
 
+TGraf *CitireGraf(FILE *f)
+{
+	if (!f) {
+		return NULL;
+	}
+	int n = 0;
+	fscanf(f, "%d", &n);
+	TGraf *g = InitGraf(n);
+	if (!g) {
+		return NULL;
+	}
+	int x = 0;
+	int y = 0;
+	while (fscanf(f, "%d %d", &x, &y) == 2) {
+		if (InserareArc(g, x, y) == -1) {
+			DistrugeGraf(&g);
+			return NULL;
+		}
+	}
+	return g;
+}
+
 int main(void)
 {
+	FILE *f = fopen("graf.txt", "r");
+	if (!f) {
+		return -1;
+	}
+	TGraf *g = CitireGraf(f);
+	if (!g) {
+		fclose(f);
+		return -1;
+	}
+	DistrugeGraf(&g);
+	fclose(f);
 	return 0;
 }
 
