b6aa0db170ef87f68f95f20a8ba632c1220d6a20
Cristian Mocanu <mocanuc579@gmail.com>
2026-06-08 23:09:03 +0300

    Adauga functie de alocare memorie.

--- ListeGenerice/main.c
+++ ListeGenerice/main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct celulag{
 	void *info;
 	struct celulag *urm;
 }TCelulaG, *TLG;
+
+TLG AlocCelulaG(void *info)
+{
+	if (!info) {
+		return NULL;
+	}
+	TLG aux = malloc(sizeof(TCelulaG));
+	if (!aux) {
+		return NULL;
+	}
+	aux->info = info;
+	aux->urm = NULL;
+	return aux;
+}
 
 int main(void)
 {
