50ac67f24d92cd3b9261a40385f383b44480bf81
Cristian Mocanu <mocanuc579@gmail.com>
2026-06-15 12:13:02 +0300

    Adauga definitie celula si functie care o aloca dinamic.

--- Cozi/CoziListeDubluInlantuite.c
+++ Cozi/CoziListeDubluInlantuite.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct celula2 {
+	int info;
+	struct celula2 *pre, *urm;
+}TCelula2, *TLista2;
+
+TCelula2 *AlocCelula2(int info)
+{
+	TCelula2 *aux = malloc(sizeof(TCelula2));
+	if (!aux) {
+		return NULL;
+	}
+	aux->info = info;
+	aux->pre = NULL;
+	aux->urm = NULL;
+	return aux;
+}
+
 int main(void)
 {
 	return 0;
