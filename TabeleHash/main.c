0eb23c910b6ce76977ce1bc1c8785391ca9be811
Cristian Mocanu <mocanuc579@gmail.com>
2026-06-15 19:03:13 +0300

    Adauga definitie pentru structuri.

--- TabeleHash/main.c
+++ TabeleHash/main.c
@@ -1,5 +1,21 @@
+// Coding Style asemanator cu cel de la curs, stiu...
+// Cod scris de mana integral.
+
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct celulag {
+	void *info;
+	struct celulag *urm;
+}TCelulaG, *TListaG;
+
+typedef int (*TFHash)(void *);
+
+typedef struct {
+	int m;
+	TFHash fh;
+	TListaG *v;
+}THash;
 
 int main(void)
 {
