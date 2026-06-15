f3f4c758a9bd706905e76c3ae2b49a94126ccf7d
Cristian Mocanu <mocanuc579@gmail.com>
2026-06-15 16:26:27 +0300

    Adauga functii de inserare + extragere.

--- Cozi/CoziListeDubluInlantuiteCirculare.c
+++ Cozi/CoziListeDubluInlantuiteCirculare.c
@@ -3,14 +3,14 @@
 
 typedef struct celula2 {
 	int info;
-	struct celula *pre, *urm;
+	struct celula2 *pre, *urm;
 }TCelula2, *TLista2;
 
 typedef struct {
 	TLista2 sf; // Puteam sa aleg "inc" in loc de "sf".
 }TCoada;
 
-TCelula2 *AlocCelula(int info)
+TCelula2 *AlocCelula2(int info)
 {
 	TCelula2 *aux = malloc(sizeof(TCelula2));
 	if (!aux) {
@@ -31,6 +31,55 @@ TCoada *InitQ(void)
 	c->sf = NULL;
 	return c;
 }
+
+int InsQ(TCoada *c, int info)
+{
+	if (!c) {
+		return -1;
+	}
+	TCelula2 *aux = AlocCelula2(info);
+	aux->pre = aux;
+	aux->urm = aux;
+	if (!aux) {
+		return -1;
+	}
+	if (!c->sf) {
+		c->sf = aux;
+		return 0;
+	}
+	aux->pre = c->sf;
+	aux->urm = c->sf->urm;
+	c->sf->urm->pre = aux;
+	c->sf->urm = aux;
+	c->sf = aux;
+	return 0;
+}
+
+int ExtrQ(TCoada *c, int *aX)
+{
+	if (!c || !aX || !c->sf) {
+		return -1;
+	}
+	*aX = c->sf->urm->info;
+	if (c->sf == c->sf->urm) {
+		free(c->sf);
+		c->sf = NULL;
+		return 0;
+	}
+	TCelula2 *target = c->sf->urm;
+	c->sf->urm = target->urm;
+	target->urm->pre = c->sf;
+	free(target);
+	return 0;
+}
+
+void ResetQ(TCoada *c)
+{
+	if (!c || !c->sf) {
+		return;
+	}
+}
+
 
 int main(void)
 {
