0eba081c63d3b0828b1b0202d51d92a7fe40703d
Cristian Mocanu <mocanuc579@gmail.com>
2026-06-15 17:13:24 +0300

    Adauga functie care aloca memorie pentru nod.

--- ArboriMulticai/main.c
+++ ArboriMulticai/main.c
@@ -1,7 +1,22 @@
 // First child, next sibling.
 
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct node {
 	int info;
 	struct node *left, *right;
 } Node, *Tree;
+
+Tree CreateNode(int info)
+{
+	Tree aux = malloc(sizeof(Node));
+	if (!aux) {
+		return NULL;
+	}
+	aux->info = info;
+	aux->left = NULL;
+	aux->right = NULL;
+	return aux;
+}
 
