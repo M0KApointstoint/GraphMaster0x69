3524d9e07bf05da6c32c260f3c13d780259496d8
Cristian Mocanu <mocanuc579@gmail.com>
2026-06-15 16:02:36 +0300

    Adauga functie care aloca celula.

--- Cozi/CoziListeDubluInlantuiteCirculare.c
+++ Cozi/CoziListeDubluInlantuiteCirculare.c
@@ -10,6 +10,18 @@ typedef struct {
 	TLista2 sf; // Puteam sa aleg "inc" in loc de "sf".
 }TCoada;
 
+TCelula2 *AlocCelula(int info)
+{
+	TCelula2 *aux = malloc(sizeof(TCelula2));
+	if (!aux) {
+		return NULL;
+	}
+	aux->info = info;
+	aux->pre = NULL;
+	aux->urm = NULL;
+	return aux;
+}
+
 int main(void)
 {
 	return 0;
